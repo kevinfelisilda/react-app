835fd20ff47998d561fb24c3d183a7c260f3e6b5
Kevin Felisilda <kevinfelisilda@gmail.com>
2018-07-17 15:16:23 +0800

    Add dashboard widgets

---
+++ src/components/widgets/UsageSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const UsageSummary = () => (
+  <div>
+    <div>Header</div>
+    <center>
+      <h3>Value</h3>
+      <div>Sub</div>
+      <p>Notes</p>
+    </center>
+  </div>
+);
+
+export default UsageSummary;
